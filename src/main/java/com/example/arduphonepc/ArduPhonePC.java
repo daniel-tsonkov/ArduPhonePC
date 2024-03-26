b54b391d6a9712f3e83ebecb85c068b5ae50cd57
dan <daniel_tzonkov@abv.bg>
2024-03-26 15:53:43 +0200

    26.03.2024

--- src/main/java/com/example/arduphonepc/ArduPhonePC.java
+++ src/main/java/com/example/arduphonepc/ArduPhonePC.java
@@ -11,7 +11,7 @@ public class ArduPhonePC extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ArduPhonePC.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());//Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");//Test
         stage.setScene(scene);
         stage.show();
